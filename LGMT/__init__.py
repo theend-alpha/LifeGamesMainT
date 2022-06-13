a7435edf05f63bf4a08114686b6b17ae44687b33
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-13 20:34:32 +0530

    Update __init__.py

--- LGMT/__init__.py
+++ LGMT/__init__.py
@@ -1,8 +1,8 @@
-from decouple import config 
+import os
 from telethon import TelegramClient, events
 
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH", default=None)
+BOT_TOKEN = os.getenv("BOT_TOKEN", default=None)
 
 ALF = TelegramClient('ALF', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
