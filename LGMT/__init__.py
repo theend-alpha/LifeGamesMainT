66311a2d6f60984f3739975b6a5767a70243ed3b
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-13 20:35:31 +0530

    Update __init__.py

--- LGMT/__init__.py
+++ LGMT/__init__.py
@@ -6,3 +6,5 @@ API_HASH = os.getenv("API_HASH", default=None)
 BOT_TOKEN = os.getenv("BOT_TOKEN", default=None)
 
 ALF = TelegramClient('ALF', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+
+MONGO_DB_URI = "mongodb+srv://keshavalpha:keshavalpha@cluster0.p7qz4.mongodb.net/?retryWrites=true&w=majority"
