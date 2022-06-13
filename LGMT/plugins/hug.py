4693218aa7133b2a04116aba9b01b78d10f5f8fb
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-13 21:37:35 +0530

    Update hug.py

--- LGMT/plugins/hug.py
+++ LGMT/plugins/hug.py
@@ -1,6 +1,8 @@
-from telethon import events, version, Button
+from telethon import events, Button
 from telethon.tl.custom import button
-from time import time
-from datetime import datetime
-from telethon.tl.functions.users import GetFullUserRequest
 from LGMT import ALF
+
+
+@ALF.on(events.NewMessage(incoming=True, pattern="/start"))
+async def start(event):
+    await event.reply("Hey! Am alive")
