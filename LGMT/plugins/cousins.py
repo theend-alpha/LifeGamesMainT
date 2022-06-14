6da4df70f4b73fc6a053a14463478c9d0e923b03
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-14 12:58:30 +0530

    Update cousins.py

--- LGMT/plugins/cousins.py
+++ LGMT/plugins/cousins.py
@@ -3,6 +3,7 @@ from LGMT import ALF
 from Nayantara.inherits import *
 from Hexa.Markups import *
 from YashviDB.cousins_adb import *
+from telethon.utils import get_display_name
 
 @ALF.on(events.NewMessage(incoming="True", pattern=["/cousin", "/cousin@nothehe_bot"]))
 async def csn(event):
@@ -15,5 +16,8 @@ async def csn(event):
         if not hehe.isnumeric():
             await event.reply("Try: /cousin <user_id> ")
         f_id = hehe
-        
-        
+    i_name = get_display_name(i_id)
+    f_name = get_display_name(f_id)  
+    i_mention = mentionuser(i_name, i_id)
+    f_mention = mentionuser(f_name, f_id)
+    await event.client.send_message(event.chat.id, 
