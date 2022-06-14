01a222287df084254017dbf19db7444130782e17
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-14 09:47:36 +0530

    Update __init__.py

--- YashviDB/__init__.py
+++ YashviDB/__init__.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
+import os
 
 DATABASE_URL = os.environ.get('DATABASE_URL', None)
 
