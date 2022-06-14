9e39e7aa307af78ea052512dea4aa32142d34772
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-14 22:34:56 +0530

    Update cousins.py

--- LGMT/plugins/cousins.py
+++ LGMT/plugins/cousins.py
@@ -15,6 +15,20 @@ i_mention = None
 f_mention = None
 i_id = None
 
+async def get_user(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.forward:
+            replied_user = await event.client(
+                GetFullUserRequest(previous_message.forward.sender_id)
+            )
+        else:
+            replied_user = await event.client(
+                GetFullUserRequest(previous_message.sender_id)
+            )
+    target = replied_user.user.id
+    return target
+
 @ALF.on(events.NewMessage(incoming="True", pattern="/cousin"))
 async def csn(event):
     global f_id
