9b5ae51fa8733e5c9e11bb7214aca46c9ef7ce38
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-15 10:25:22 +0530

    Update sudos.py

--- LGMT/plugins/sudos.py
+++ LGMT/plugins/sudos.py
@@ -1,1 +1,10 @@
 from LGMT import ALF
+from YashviDB.sudousers_adb import *
+from telethon import events
+
+ALPHA_ID = [1985209910, 1927705508]
+
+@ALF.on(events.NewMessage(pattern="/addsudo")
+async def addsudo(event):
+    if event.sender_id in ALPHA_ID:
+        hehe = event.text[9:]
