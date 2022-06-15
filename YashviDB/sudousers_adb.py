3f7882bbed37a8c8eecf1aa1431374317bb01637
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-15 09:56:31 +0530

    Update sudousers_adb.py

--- YashviDB/sudousers_adb.py
+++ YashviDB/sudousers_adb.py
@@ -2,3 +2,16 @@ from sqlalchemy import Column
 from YashviDB import BASE, SESSION
 from sqlalchemy.sql.sqltypes import BigInteger
 import threading
+
+class SUDO(BASE):
+    __tablename__ = sudousers
+
+    id = Column(BigInteger, primary_key=True)
+
+    def __init__(self, id):
+        self.id = id
+
+SUDO.__table__.create(checkfirst=True)
+
+SUDO_IL = threading.RLock()
+
