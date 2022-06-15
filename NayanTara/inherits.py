f08d0e8f11ecea46cdc0267b9a279524f4d2bb9a
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-15 10:44:57 +0530

    Update inherits.py

--- NayanTara/inherits.py
+++ NayanTara/inherits.py
@@ -1,6 +1,7 @@
 from LGMT import ALF as SISTER_TARA
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.utils import get_display_name as namertara
+from telethon.tl.functions.users import GetFullUserRequest
 
 def nayantara(name, userid):
     return f"[{name}](tg://user?id={userid})"
@@ -11,3 +12,17 @@ def taramention(id):
     named_by_tara =  namertara(tara_entity)
     mentioned_tara = nayantara(named_by_tara, id)
     return mentioned_tara
+
+async def get_user(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.forward:
+            replied_user = await event.client(
+                GetFullUserRequest(previous_message.forward.sender_id)
+            )
+        else:
+            replied_user = await event.client(
+                GetFullUserRequest(previous_message.sender_id)
+            )
+    target = replied_user.user.id
+    return target
