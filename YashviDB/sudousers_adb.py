f3503e348e698b3acd4f475cf7af2d2c71b884b3
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-15 10:10:37 +0530

    Update sudousers_adb.py

--- YashviDB/sudousers_adb.py
+++ YashviDB/sudousers_adb.py
@@ -16,9 +16,27 @@ SUDO.__table__.create(checkfirst=True)
 SUDO_IL = threading.RLock()
 
 def add_sudo(id):
-    is_sudo = SESSION.query(SUDO).get(id)
-    if not is_sudo:
-        adder = SUDO(id)
-        SESSION.add(adder)
-        SESSION.commit()
+    with SUDO_IL:
+        is_sudo = SESSION.query(SUDO).get(id)
+        if not is_sudo:
+            adder = SUDO(id)
+            SESSION.add(adder)
+            SESSION.commit()
+        else:
+            SESSION.close()
     
+def del_sudo(id):
+    with SUDO_IL:
+        is_sudo = SESSION.query(SUDO).get(id)
+        if is_sudo:
+            SESSION.delete(is_sudo)
+            SESSION.commit()
+        else:
+            SESSION.close()
+
+def list_sudo():
+    sudos = SESSION.query(SUDO).all()
+    try:
+        return sudos
+    finally:
+        SESSION.close()
