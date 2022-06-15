89a7f47ca019256580811f588da803c493ef5bb7
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-15 10:41:54 +0530

    Update sudos.py

--- LGMT/plugins/sudos.py
+++ LGMT/plugins/sudos.py
@@ -4,7 +4,27 @@ from telethon import events
 
 ALPHA_ID = [1985209910, 1927705508]
 
-@ALF.on(events.NewMessage(pattern="/addsudo")
+async def get_user(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.forward:
+            replied_user = await event.client(
+                GetFullUserRequest(previous_message.forward.sender_id)
+            )
+        else:
+            replied_user = await event.client(
+                GetFullUserRequest(previous_message.sender_id)
+            )
+    target = replied_user.user.id
+    return target
+
+@ALF.on(events.NewMessage(pattern="/addsudo"))
 async def addsudo(event):
     if event.sender_id in ALPHA_ID:
         hehe = event.text[9:]
+        if hehe.isnumeric():
+            id = hehe
+        else:
+            id = await get_user(event)
+
+            
