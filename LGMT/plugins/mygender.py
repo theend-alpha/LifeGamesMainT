def3e78a02ea400cc9fc1cad507ad510b2cef5c7
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-07-03 16:17:06 +0530

    Update mygender.py

--- LGMT/plugins/mygender.py
+++ LGMT/plugins/mygender.py
@@ -3,3 +3,11 @@ from telethon import events, Button
 from telethon.tl.custom import button
 from YashuAlpha.genders import *
 from Hexa import *
+from NayanTara.inherits import *
+from telethon.utils import get_display_name as rupali
+
+@ALF.on(events.NewMessage(incoming=True, pattern="/mygender"))
+async def smexy(event):
+    a = event.sender_id
+    alpha_entity = await ALF.get_entity(a)
+    a_fn = rupali(alpha_entitiy)
