924826713c03b3856c913a13d10997a3c3917ca6
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-07-03 15:19:50 +0530

    Update cousins.py

--- YashuAlpha/cousins.py
+++ YashuAlpha/cousins.py
@@ -2,4 +2,11 @@ from YashuAlpha import YashviLovesAlpha
 
 cousinsdb = YashviLovesAlpha.cousins
 
-async def add_cousin(a: int, b:int):
+async def add_cousin(a: int, b: int):
+    _cousins = await get_cousins(a)
+    if b in _cousins:
+        return
+    _cousins.append(b)
+    await cousinsdb.update_one(
+        {"a": a}, {"$set": {"cousins": _cousins}}, upsert=True
+    )
