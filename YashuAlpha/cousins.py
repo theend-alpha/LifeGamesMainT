510c83b18cb1c341f99f720479c89d9d5fb4d95f
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-07-03 15:28:43 +0530

    Update cousins.py

--- YashuAlpha/cousins.py
+++ YashuAlpha/cousins.py
@@ -10,3 +10,28 @@ async def add_cousin(a: int, b: int):
     await cousinsdb.update_one(
         {"a": a}, {"$set": {"cousins": _cousins}}, upsert=True
     )
+
+async def get_cousins(a: int):
+    _cousins = await cousinsdb.find_one({"a": a})
+    if _cousins:
+        cousins = _cousins["cousins"]
+    else:
+        cousins = {}
+    return cousins
+
+async def del_cousin(a:int, b: int):
+    _cousins = await get_cousins(a)
+    if b in _cousins:
+        _cousins.remove(b)
+    else:
+        return
+    await cousinsdb.update_one(
+        {"a": a}, {"$set": {"cousins": _cousins}}, upsert=True
+    )
+
+async def are_cousins(a: int, b: int):
+    _cousins = await cousinsdb.find_one({"a": a})
+    if b in _cousins:
+        return True
+    else:
+        False
