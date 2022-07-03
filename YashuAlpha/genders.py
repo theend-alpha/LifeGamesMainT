6a9ed3f2ffd6105c99cda4158b09ce3d6e8f75a3
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-07-03 15:37:59 +0530

    Update genders.py

--- YashuAlpha/genders.py
+++ YashuAlpha/genders.py
@@ -1,3 +1,12 @@
 from YashuAlpha import YashviLovesAlpha
 
 genderdb = YashviLovesAlpha.gender
+
+async def add_male(a: int):
+    males = await get_males()
+    if a in males:
+        return
+    males.append(a)
+    await genderdb.insert_one({"males": males})
+
+async def 
